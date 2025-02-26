0b1b270562da11596376d63fe7fa4d2f0a21e0fe
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-26 15:23:04 +0500

    Ai-agent

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from llama_index.core.embeddings import resolve_embed_model
 # Initialize Ollama with the 'mistral' model
 llm = Ollama(model="mistral", request_timeout=30.0)
 
-# Run a prompt
-response = llm.complete("hello world")
 
-# Print the response
-print(response.text)
+parser = LlamaParse(result_type="markdown")
+
+file_extractor = {".pdf":parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
