9c766570707ec7ffcd7f59b91d0cff4f720148cc
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-27 19:54:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from llama_index.llms.ollama import Ollama
+import ollama
 from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from dotenv import load_dotenv
+from llama_index.core.agent import ReActAgent
+load_dotenv()
 # Initialize Ollama with the 'mistral' model
-llm = Ollama(model="mistral", request_timeout=30.0)
+import ollama
+
+response = ollama.chat(model="mistral", messages=[{"role": "user", "content": "Hello"}])
+print(response)
+
 
 
 parser = LlamaParse(result_type="markdown")
@@ -16,4 +24,16 @@ embed_model = resolve_embed_model("local:BAAI/bge-m3")
 vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
 
 query_engine = vector_index.as_query_engine(llm=llm)
-query_engine.query("what are some of the route in the api")
+
+tools = [
+    QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name = "api_documentation",
+            description="this give documentation about code for an API. use this for reading docs for the API"
+
+        )
+    )
+]
+code_llm = Ollama(model="codellama")
+agent = ReActAgent.from_tools(tools, llm=code_llm , verbose=True, context=)
