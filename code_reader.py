a14854efd4c2ee02095503c10ab01fc68bc73b0f
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-27 20:10:12 +0500

    Update code_reader.py

--- code_reader.py
+++ code_reader.py
@@ -1,1 +1,10 @@
 
+from llama_index.core.tools import FunctionTool
+import os
+
+def code_reader_func(file_name):
+    path = os.path.join("data", file_name)
+
+    try:
+        with open (path, "r") as f:
+            content = f.read()
