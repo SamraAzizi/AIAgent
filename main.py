dfb4e65244e5aa233a518ae8deb31bc6f520627a
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-28 12:33:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from dotenv import load_dotenv
 from llama_index.core.agent import ReActAgent
+from prompts import context
+from code_reader import code_reader
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
 load_dotenv()
 # Initialize Ollama with the 'mistral' model
 import ollama
@@ -32,8 +37,17 @@ tools = [
             name = "api_documentation",
             description="this give documentation about code for an API. use this for reading docs for the API"
 
-        )
-    )
+        ),
+    ),
+    code_reader,
 ]
 code_llm = Ollama(model="codellama")
-agent = ReActAgent.from_tools(tools, llm=code_llm , verbose=True, context=)
+agent = ReActAgent.from_tools(tools, llm=code_llm , verbose=True, context=context)
+
+class CodeOutPut(BaseModel):
+    
+
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
