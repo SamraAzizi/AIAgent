40d481eaf0079c5f2334c5bc345382a99015f309
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-28 22:25:33 +0500

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,7 +1,8 @@
+
 context = """ purpose: the primary role of this agent is to assist user by analyzing code. it should be able to generate code and answer the question about the provided code"""
 
-
-"""
+code_parser_template ="""
 Parse the response from a previous LLM into a description and a string of valid code, 
 also come up with a valid filename this could be saved as that doesnt containe specila characters
+Here is the response: {response}. You should parse this in the following JSON formate: 
 """
